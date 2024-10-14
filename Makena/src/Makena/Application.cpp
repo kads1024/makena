354e670fe35fe7848b4e3d95dceb0b07dfe0413c
ken.santos <kenneth.santos@secret6.com>
2024-10-14 17:21:45 +0800

    Setup Window Events using Event System so that there is modularity between the two system

--- Makena/src/Makena/Application.cpp
+++ Makena/src/Makena/Application.cpp
@@ -1,20 +1,31 @@
 #include "mknpch.h"
 #include "Application.h"
 
-#include "Makena/Events/ApplicationEvent.h"
 #include "Makena/Log.h"
 
 #include "GLFW/glfw3.h"
 
 namespace Makena
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		MKN_CORE_TRACE(e.ToString());
 	}
 
 	void Application::Run()
@@ -27,4 +38,11 @@ namespace Makena
 			m_Window->OnUpdate();
 		}
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+	
 }
